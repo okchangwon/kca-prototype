1b9bbf639e401b5be24a59a6de5fa6df56c4c063
ok-changwon <ok.changwon@navercorp.com>
2020-11-08 21:19:52 +0900

    fix: hash 모드로 변경

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Uploader from "@/components/Uploader.vue";
 import Result from "@/components/Result.vue";
 
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
